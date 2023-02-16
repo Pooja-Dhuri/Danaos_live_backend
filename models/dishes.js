dca56cf7ec0bd25a500cb1fec834527f93ad0e32
VishalBhuse <vbhuse1997@gmail.com>
2023-02-16 17:54:29 +0530

    dish versionkey

--- models/dishes.js
+++ models/dishes.js
@@ -1,12 +1,18 @@
-const {Schema,model}=require("mongoose");
+const { Schema, model } = require("mongoose");
 
-const DishSchema=new Schema({
-    imageUrl:{type:String,required:true},
-    dishName:{type:String,required:true},
-    rating:{type:Number,required:true},
-    price:{type:Number,required:true},
-    type:{type:String,required:true}
-})
+const DishSchema = new Schema(
+  {
+    imageUrl: { type: String, required: true },
+    dishName: { type: String, required: true },
+    rating: { type: Number, required: true },
+    price: { type: Number, required: true },
+    type: { type: String, required: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
-const DishesData=model("DishesData",DishSchema);
-module.exports=DishesData;
+const DishesData = model("DishesData", DishSchema);
+module.exports = DishesData;
